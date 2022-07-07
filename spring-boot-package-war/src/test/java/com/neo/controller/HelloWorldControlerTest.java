961ce15fb3232b3b06896b30853f38c43c693aac
Oded3012 <96193529+Oded3012@users.noreply.github.com>
2022-07-08 01:04:32 +0300

    Update HelloWorldControlerTest.java

--- spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTest.java
+++ spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloWorldControllerTest {
+public class HelloWorldControllerTest.java {
 
     @Autowired
     private MockMvc mockMvc;
