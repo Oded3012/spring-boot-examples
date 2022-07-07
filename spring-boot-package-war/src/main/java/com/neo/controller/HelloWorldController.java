b797c487fca81f8901257c11da3420fd79dcccca
Oded3012 <96193529+Oded3012@users.noreply.github.com>
2022-07-08 00:53:34 +0300

    Update HelloWorldController.java

--- spring-boot-package-war/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-package-war/src/main/java/com/neo/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
-package com.neo.controller;
+package com.example.helloworld.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World xx";
+
+    @GetMapping("/hello")
+    public String sendGreetings() {
+        return "Hello, World!";
     }
 }
